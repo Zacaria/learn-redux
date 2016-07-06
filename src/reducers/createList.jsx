36f7bc69768709bbbb0db2ceaccd91df970699da
Zacaria <zchtatar@excilys.com>
2016-07-06 19:31:14 +0200

    [27] Updating data on server

--- src/reducers/createList.jsx
+++ src/reducers/createList.jsx
@@ -1,53 +1,67 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 
 const createList = (filter) => {
+
+  const handleToggle = (state, action) => {
+    const { result: toggledId, entities } = action.response;
+    const { completed } = entities.todos[toggledId];
+    const shouldRemove = (
+      (completed && filter === 'active') ||
+      (!completed && filter === 'complted')
+    )
+
+    return shouldRemove ?
+      state.filter(id => id !== toggledId) :
+      state;
+  };
   const ids = (state = [], action) => {
-    switch (action.type) {
-      case 'FETCH_TODOS_SUCCESS':
-        return filter === action.filter
-          ? action.response.result
-          : state;
-      case 'ADD_TODO_SUCCESS':
-        return filter !== 'completed'
-          ? [
+    switch(action.type) {
+    case 'FETCH_TODOS_SUCCESS':
+      return filter === action.filter ?
+        action.response.result :
+        state;
+    case 'ADD_TODO_SUCCESS':
+      return filter !== 'completed' ? [
             ...state,
             action.response.result
-          ]
-          : state;
-      default:
-        return state;
+          ] :
+        state;
+    case 'TOGGLE_TODO_SUCCESS':
+      return handleToggle(state, action);
+    default:
+      return state;
     }
   };
 
   const isFetching = (state = false, action) => {
-    if (action.filter !== filter)
+    if(action.filter !== filter)
       return state;
-    switch (action.type) {
-      case 'FETCH_TODOS_REQUEST':
-        return true;
-      case 'FETCH_TODOS_SUCCESS':
-      case 'FETCH_TODOS_FAILURE':
-        return false;
-      default:
-        return state;
+    switch(action.type) {
+    case 'FETCH_TODOS_REQUEST':
+      return true;
+    case 'FETCH_TODOS_SUCCESS':
+    case 'FETCH_TODOS_FAILURE':
+      return false;
+    default:
+      return state;
     }
   };
 
   const getErrorMessage = (state = null, action) => {
-    if (action.filter !== filter)
+    if(action.filter !== filter)
       return state;
-    switch (action.type) {
-      case 'FETCH_TODOS_FAILURE':
-        return action.message;
-      case 'FETCH_TODOS_SUCCESS':
-      case 'FETCH_TODOS_REQUEST':
-        return null;
-      default:
-        return state;
+    switch(action.type) {
+    case 'FETCH_TODOS_FAILURE':
+      return action.message;
+    case 'FETCH_TODOS_SUCCESS':
+    case 'FETCH_TODOS_REQUEST':
+      return null;
+    default:
+      return state;
     }
   };
 
-  return combineReducers({ids, isFetching, getErrorMessage});
+  return combineReducers({ ids, isFetching, getErrorMessage });
 };
 
 export default createList;
